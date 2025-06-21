8fb91c626161d309fa7b6727f2e7bdfcb5a70808
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2025-06-20 20:06:30 -0600

    Update Postfix.py

--- Postfix.py
+++ Postfix.py
@@ -5,46 +5,55 @@ Functions used to perform the Shunting Yard algorithm
 precedence:dict = {'|': 1, '.': 2, '*': 3, '+' : 3, '?' : 3}
 
 def is_operator(token: str) -> bool:
-        # check if the operator keys are here
-        return token in precedence.keys()
+    # check if the operator keys are here
+    return token in precedence.keys()
 
 
 def has_higher_precedence(op1: str, op2: str) -> bool:
-        # checks if the precedence of the operator on the right is bigger than the one on the left
-        return precedence[op1] > precedence[op2]
+    # checks if the precedence of the operator on the right is bigger than the one on the left
+    return precedence[op1] > precedence[op2]
+
+def is_token(chr: str) -> bool:
+    return not is_operator(chr) and chr not in {'(', ')'}
 
 
 def shunting_yard(expression: list[str]) -> list[str]:
-        output: list[str] = []
-        operator_stack: list[str] = []
+    '''
+    Uses Shunting-Yard algorithm
+    '''
+    output: list[str] = []
+    operator_stack: list[str] = []
 
+    for token in expression:
+        # if token is not an operator, and not a parenthesis
+        #if token.isalnum():  
+        if is_token(token):
+            output.append(token)
+        # if the token is operator
+        elif is_operator(token):
+            # check if the token 
+            while (operator_stack and is_operator(operator_stack[-1]) and has_higher_precedence(operator_stack[-1], token)):
+                output.append(operator_stack.pop())
+            operator_stack.append(token)
 
-        for token in expression:
-            # if token is alphanumeric
-            if token.isalnum():  
-                output.append(token)
-            # if the token is operator
-            elif is_operator(token):
-                # check if the token 
-                while (operator_stack and is_operator(operator_stack[-1]) and has_higher_precedence(operator_stack[-1], token)):
-                    output.append(operator_stack.pop())
-                operator_stack.append(token)
+        elif token == '(':  
+            operator_stack.append(token)
 
-            elif token == '(':  
-                operator_stack.append(token)
+        elif token == ')':  
+            while operator_stack and operator_stack[-1] != '(':
+                output.append(operator_stack.pop())
+            if operator_stack and operator_stack[-1] == '(':
+                operator_stack.pop()
 
-            elif token == ')':  
-                while operator_stack and operator_stack[-1] != '(':
-                    output.append(operator_stack.pop())
-                if operator_stack and operator_stack[-1] == '(':
-                    operator_stack.pop()
+    while operator_stack:
+        output.append(operator_stack.pop())
 
-        while operator_stack:
-            output.append(operator_stack.pop())
-
-        return output
+    return output
 
 def place_implicit_concat(regex: str) -> list[str]:
+    '''
+    Place implicit symbol '.' for concatenation to simplify token handling
+    '''
     tokens: list[str] = []
     prev: str | None = None
     left_side: set = {')', '*', '+', '?'}
@@ -53,8 +62,8 @@ def place_implicit_concat(regex: str) -> list[str]:
         token = char
         # check if prev is None before using isalnum()
         if prev is not None:
-            left_cond:bool = (prev.isalnum() or prev in left_side)
-            right_cond:bool = (token.isalnum() or token in right_side)
+            left_cond:bool = (is_token(prev) or (prev in left_side))
+            right_cond:bool = (is_token(token) or (token in right_side))
             # if it satisfies both conditions, append a concatenation
             if left_cond and right_cond:
                 tokens.append('.')
